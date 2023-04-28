5270fd82a219bd94edeaeeb2acec390755a3081f
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2023-04-28 13:51:52 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYous = [];
+    for (let i = 0; i <= names.length - 1; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYous;
+}
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    };
+}
